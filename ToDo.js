91496cd0d9aee09cdde60adfeba3d0f0d576db4a
MargaretBourlon <mdbourlon@gmail.com>
2016-04-19 23:44:08 +0100

    prototype fix

--- ToDo.js
+++ ToDo.js
@@ -3,7 +3,7 @@ var task = function (name, rate) {
   this.name = name;
   this.rate = rate;
 };
-var addItem = function (NAME, RATE) {
-  var myTask = new task(this.NAME, this.RATE);
-  myList.push(myTask);
+var addItem = function (TITLE, NAME, RATE) {
+  this.title = new task(NAME, RATE);
+  myList[myList.length] = this.title;
 };
