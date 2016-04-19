5f23d0bf9c995fb9bf5ee418eb8c596e04362d99
MargaretBourlon <mdbourlon@gmail.com>
2016-04-19 11:51:00 +0100

    Task prototype

--- ToDo.js
+++ ToDo.js
@@ -1,3 +1,9 @@
-//js nonsense
-var name = prompt("What is your name?");
-console.log(name);
+var myList = [];
+var task = function (name, rate) {
+  this.name = name;
+  this.rate = rate;
+};
+var addItem = function (NAME, RATE) {
+  var myTask = new task(this.NAME, this.RATE);
+  myList.push(myTask);
+};
